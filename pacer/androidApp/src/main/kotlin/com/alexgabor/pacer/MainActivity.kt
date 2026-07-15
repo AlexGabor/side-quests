e5332193cecfa5af3ad5b121c33ae27c8ffd6ce2
Alex Gabor <alex.dan.gabor@gmail.com>
2026-07-15 11:06:17 +0300

    Set status bar icons to dark

--- pacer/androidApp/src/main/kotlin/com/alexgabor/pacer/MainActivity.kt
+++ pacer/androidApp/src/main/kotlin/com/alexgabor/pacer/MainActivity.kt
@@ -5,7 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +16,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+            val view = LocalView.current
+            SideEffect {
+                WindowCompat.getInsetsController(window, view)
+                    .isAppearanceLightStatusBars = true
+            }
             App()
         }
     }
