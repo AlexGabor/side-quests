1d72393128bdc3cec972ded8d5c66b1f6b7385e7
Alex Gabor <alex.dan.gabor@gmail.com>
2026-07-15 11:05:57 +0300

    Delete existing content

--- pacer/shared/src/commonMain/kotlin/com/alexgabor/pacer/GreetingUtil.kt
+++
@@ -1,4 +0,0 @@
-package com.alexgabor.pacer
-
-fun sayHello(to: String): String =
-    "Hello, $to!"
--- pacer/shared/src/commonMain/kotlin/com/alexgabor/pacer/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.alexgabor.pacer
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
