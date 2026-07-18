354b84cc0c1444ae012bb194f94d8562bc1942d6
Alex Gabor <alex.dan.gabor@gmail.com>
2026-07-18 14:01:08 +0300

    Draw selected value on top

--- pacer/shared/src/commonMain/kotlin/com/alexgabor/pacer/Slider.kt
+++ pacer/shared/src/commonMain/kotlin/com/alexgabor/pacer/Slider.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -15,6 +16,9 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
@@ -94,6 +98,16 @@ fun <T> Track(
                     }
                 }
             }
+        }
+        Box(
+            modifier = Modifier.padding(start = firstGuideline, top = verticalPadding)
+                .graphicsLayer {
+                    translationX = -size.width / 2f
+                }
+                .background(Color.White)
+        ) {
+            val index by remember { derivedStateOf {  listState.firstVisibleItemIndex } }
+            itemContent(trackItems[index])
         }
     }
 }
