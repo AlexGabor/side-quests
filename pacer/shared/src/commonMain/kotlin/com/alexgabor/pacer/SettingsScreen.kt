25496b2b5130942e6c733ae3df38eda9b18f2704
Alex Gabor <alex.dan.gabor@gmail.com>
2026-09-03 11:56:27 +0300

    Switch to weblocal storage

--- pacer/shared/src/commonMain/kotlin/com/alexgabor/pacer/SettingsScreen.kt
+++ pacer/shared/src/commonMain/kotlin/com/alexgabor/pacer/SettingsScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -37,7 +38,7 @@ fun rememberSettingsScreenState(
     settings: PacerSettingsRepository = rememberPacerSettingsRepository(),
     coroutineScope: CoroutineScope = rememberCoroutineScope(),
 ): SettingsScreenState {
-    return SettingsScreenState(settings, coroutineScope)
+    return remember(settings, coroutineScope) { SettingsScreenState(settings, coroutineScope) }
 }
 
 class SettingsScreenState(
