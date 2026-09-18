391fb345abdffa3d1754c26d7ba2876368d89a29
Alex Gabor <alex.dan.gabor@gmail.com>
2026-09-18 19:01:01 +0300

    Bump version

--- pacer/androidApp/build.gradle.kts
+++ pacer/androidApp/build.gradle.kts
@@ -10,8 +10,8 @@ android {
 
     defaultConfig {
         applicationId = "com.alexgabor.pacer"
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
     }
 }
 
